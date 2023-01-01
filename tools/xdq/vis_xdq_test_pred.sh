a6e08fdf0346185a6bc11b3769ee99eaee8e47e1
正甲 <lizhengjia.lzj@alibaba-inc.com>
2023-01-01 23:07:40 +0800

    rm unused sh

--- tools/xdq/vis_xdq_test_pred.sh
+++
@@ -1,7 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=1 tools/xdq/visualize.py \
-configs/bevfusion/lidar_stream/hv_pointpillars_secfpn_sbn-all_4x8_2x_xdq-3d.py \
---launcher pytorch \
---checkpoint work_dirs/hv_pointpillars_secfpn_sbn-all_4x8_2x_xdq-3d/latest.pth \
---mode pred \
---out-dir vis_xdq_test_pred \
---bbox-score $1 ${@:2}
--- tools/xdq/vis_xdq_train_gt.sh
+++
@@ -1,5 +0,0 @@
-python tools/xdq/visualize.py \
-configs/bevfusion/lidar_stream/hv_pointpillars_secfpn_sbn-all_4x8_2x_xdq-3d.py \
---mode gt \
---split train \
---out-dir vis_xdq_train_gt
